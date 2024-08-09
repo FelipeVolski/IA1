45e750d7f1de5bacb604c1abaeb4a47f40d72368
FelipeVolski <160855622+FelipeVolski@users.noreply.github.com>
2024-08-09 11:57:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,10 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas =[
     {
-        enunciado: "Pergunta 1",
-        alternativas:["Alternativa 1", "Alternativa 2"],
-    
-    },
-    {
-        enunciado: "Pergunta 2",
-        alternativas:["Alternativa 1", "Alternativa 2"],
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco ",
+        alternativas: {
+            {
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema 
     },
     {
         enunciado: "Pergunta 3",
@@ -23,6 +20,7 @@ const perguntas =[
 
 let atual = 0;
 let perguntaAtual;
+let historiafinal = ""/""/
 
 function mostraPerguntas(){
     perguntaAtual = perguntas[atual];
